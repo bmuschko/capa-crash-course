946ff89b64c807900c322b3a3a1c4e00e0f6e169
Benjamin Muschko <benjamin.muschko@gmail.com>
2024-04-02 15:50:56 -0600

    Use ApiSix for traffic routing

---
+++ exercises/argo-rollouts/03-canary-deployment/app/index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ "version": "2.0.0" }));
+});
+
+server.listen(port);
